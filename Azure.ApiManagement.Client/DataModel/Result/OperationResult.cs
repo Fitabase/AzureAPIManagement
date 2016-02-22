a675adf5f9ab7f8b81b5ec50215bb94480e66b6b
Adriyan Vladimirov <adro@adrianvladimirov.com>
2016-02-22 22:07:38 +0200

    Base class for result of operations with no data retrieval

---
+++ Azure.ApiManagement.Client/DataModel/Result/OperationResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallStepsLabs.Azure.ApiManagement.Model
+{
+    public interface IOperationResult
+    {
+        HttpStatusCode StatusCode { get; set; }
+
+        string StatusMessage { get; set; }
+
+        bool IsSuccessfull();
+    }
+
+    public class OperationResult : IOperationResult
+    {
+        public HttpStatusCode StatusCode { get; set; }
+
+        public string StatusMessage { get; set; }
+
+        public virtual bool IsSuccessfull()
+        {
+            return true;
+        }
+    }
+}
