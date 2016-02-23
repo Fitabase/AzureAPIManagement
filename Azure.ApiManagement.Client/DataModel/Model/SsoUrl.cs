672392c260787563e0ae61f311dce4d9c00fc642
Adriyan Vladimirov <adro@adrianvladimirov.com>
2016-02-23 04:11:59 +0200

    Fixes to Product API methods Added back initial operations - commented temporarily

---
+++ Azure.ApiManagement.Client/DataModel/Model/SsoUrl.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace SmallStepsLabs.Azure.ApiManagement.Model
+{
+    public class SsoUrl
+    {
+        [JsonProperty("value")]
+        public string Value;
+    }
+}
--- Azure.ApiManagement.Client/Services/ManagementClient.cs
+++ Azure.ApiManagement.Client/Services/ManagementClient.cs
@@ -14,12 +14,114 @@ namespace SmallStepsLabs.Azure.ApiManagement
             base(host, serviceId, accessKey)
         { }
 
+        #region User Operations
+
+        public string GetRequestOperationSignature(string operation, string salt, string returnUrl = null, string productId = null, string userId = null, string subscriptionId = null)
+        {
+            throw new NotImplementedException();
+
+            //string key = _delegationValidationKey;
+            //var encoder = new HMACSHA512(Convert.FromBase64String(key));
+            //string signature;
+
+            //switch (operation)
+            //{
+            //    case "SignIn":
+            //        signature = Convert.ToBase64String(encoder.ComputeHash(Encoding.UTF8.GetBytes(salt + "\n" + returnUrl)));
+            //        break;
+            //    case "Subscribe":
+            //        signature = Convert.ToBase64String(encoder.ComputeHash(Encoding.UTF8.GetBytes(salt + "\n" + productId + "\n" + userId)));
+            //        break;
+            //    case "Unsubscribe":
+            //        signature = Convert.ToBase64String(encoder.ComputeHash(Encoding.UTF8.GetBytes(salt + "\n" + subscriptionId)));
+            //        break;
+            //    case "ChangeProfile":
+            //    case "ChangePassword":
+            //    case "SignOut":
+            //        signature = Convert.ToBase64String(encoder.ComputeHash(Encoding.UTF8.GetBytes(salt + "\n" + userId)));
+            //        break;
+            //    default:
+            //        signature = "";
+            //        break;
+            //}
+
+            //return signature;
+        }
+
+        public async Task<SsoUrl> LoginUserWithSsoUrlAsync(string userId)
+        {
+            throw new NotImplementedException();
+
+            //using (var client = new HttpClient())
+            //{
+            //    client.BaseAddress = new Uri(_ApimRestHost);
+            //    client.DefaultRequestHeaders.Add("Authorization", ApimRestAuthHeader());
+            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/json"));
+
+            //    HttpResponseMessage response = await client.PostAsync("/users/" + userId + "/generateSsoUrl?api-version=" + _ApimRestApiVersion, new StringContent("", Encoding.UTF8, "text/json"));
+            //    string httpContent = await response.Content.ReadAsStringAsync();
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        //We got an SSO token - redirect
+            //        //HttpContent receiveStream = response.Content;
+            //        //var SsoUrlJson = await receiveStream.ReadAsStringAsync();
+            //        SsoUrl su = DeserializeToJson<SsoUrl>(httpContent);
+            //        return su;
+            //    }
+            //    else
+            //    {
+            //        throw new HttpException((int)response.StatusCode, httpContent);
+            //    }
+
+            //}
+        }
+
+        public async Task<bool> CreateUserAsync(string userId, string userFirstName, string userLastName, string userEmail, string userPassword)
+        {
+            throw new NotImplementedException();
+
+            ////create user in APIM as well
+            //using (var client = new HttpClient())
+            //{
+            //    client.BaseAddress = new Uri(_ApimRestHost);
+            //    client.DefaultRequestHeaders.Add("Authorization", ApimRestAuthHeader());
+            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/json"));
+
+            //    var ApimUser = new
+            //    {
+            //        firstName = userFirstName,
+            //        lastName = userLastName,
+            //        email = userEmail,
+            //        password = userPassword,
+            //        state = "active"
+            //    };
+
+            //    var ApimUserJson = SerializeToJson(ApimUser);
+
+            //    HttpResponseMessage response = await client.PutAsync("/users/" + userId + "?api-version=" + _ApimRestApiVersion, new StringContent(ApimUserJson, Encoding.UTF8, "text/json"));
+            //    string httpContent = await response.Content.ReadAsStringAsync();
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        //User created successfully
+
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        throw new HttpException((int)response.StatusCode, httpContent);
+            //    }
+            //}
+        }
+
+        #endregion
+
         #region Products CRUD
 
         /// <summary>
         /// Get a list of all products
         /// https://msdn.microsoft.com/en-us/library/azure/dn776336.aspx#ListProducts
         /// </summary>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <returns></returns>
         public Task<List<Product>> GetProductsAsync(string filter = null, bool expandGroups = false,
             CancellationToken cancellationToken = default(CancellationToken))
@@ -47,6 +149,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// https://msdn.microsoft.com/en-us/library/azure/dn776336.aspx#GetProduct
         /// </summary>
         /// <param name="productId">Product identifier.</param>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <returns></returns>
         public Task<Product> GetProductAsync(string productId, CancellationToken cancellationToken = default(CancellationToken))
         {
@@ -85,6 +188,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// https://msdn.microsoft.com/en-us/library/azure/dn776336.aspx#UpdateProduct
         /// </summary>
         /// <param name="product"></param>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <returns></returns>
         public Task<bool> UpdateProductAsync(Product product, CancellationToken cancellationToken = default(CancellationToken))
         {
@@ -108,6 +212,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// Delete a product
         /// https://msdn.microsoft.com/en-us/library/azure/dn776336.aspx#DeleteProduct
         /// </summary>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <param name="productId">Product identifier.</param>
         /// <param name="deleteSubscriptions">Specify true to indicate that any subscriptions associated with this product should be deleted; otherwise false.If this query parameter is missing, the default is false</param>
         /// <returns></returns>
@@ -141,6 +246,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// List APIs associated with a product
         /// https://msdn.microsoft.com/en-us/library/azure/dn776336.aspx#ListAPIs
         /// </summary>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <param name="productId">Product identifier.</param>
         /// <returns></returns>
         public Task<List<API>> GetProductAPIsAsync(string productId, CancellationToken cancellationToken = default(CancellationToken))
@@ -162,6 +268,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// Adds an API to the specified product.
         /// https://msdn.microsoft.com/en-us/library/azure/dn776336.aspx#AddAPI
         /// </summary>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <param name="productId">Product identifier.</param>
         /// <param name="apiId">API identifier.</param>
         /// <returns></returns>
@@ -173,7 +280,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
             if (String.IsNullOrEmpty(apiId))
                 throw new ArgumentException("apiId is required");
 
-            var uri = String.Format("/products/{0}/apis/{0}", productId, apiId);
+            var uri = String.Format("/products/{0}/apis/{1}", productId, apiId);
             var request = base.GetRequest(uri, "PUT");
 
             return base.ExecuteRequestAsync(request, HttpStatusCode.Created, cancellationToken);
@@ -183,6 +290,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// Removes the specified API from the specified product.
         /// https://msdn.microsoft.com/en-us/library/azure/dn776336.aspx#RemoveAPI
         /// </summary>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <param name="productId">Product identifier.</param>
         /// <param name="apiId">API identifier.</param>
         /// <returns></returns>
@@ -194,10 +302,89 @@ namespace SmallStepsLabs.Azure.ApiManagement
             if (String.IsNullOrEmpty(apiId))
                 throw new ArgumentException("apiId is required");
 
-            var uri = String.Format("/products/{0}/apis/{0}", productId, apiId);
+            var uri = String.Format("/products/{0}/apis/{1}", productId, apiId);
             var request = base.GetRequest(uri, "DELETE");
 
             return base.ExecuteRequestAsync(request, HttpStatusCode.NoContent, cancellationToken);
+        }
+
+        #endregion
+
+        #region Product Subscription
+
+        public async Task<bool> AddProductSubscriptionAsync(string userId, string productId, string createSubscriptionId)
+        {
+            throw new NotImplementedException();
+
+            ////Register user for product
+            //using (var client = new HttpClient())
+            //{
+            //    client.BaseAddress = new Uri(_ApimRestHost);
+            //    client.DefaultRequestHeaders.Add("Authorization", ApimRestAuthHeader());
+            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/json"));
+
+            //    HttpResponseMessage response;
+
+            //    var ApimSubscription = new
+            //    {
+            //        userId = "/users/" + userId,
+            //        productId = "/products/" + productId,
+            //        state = "active"
+            //    };
+
+            //    var ApimSubscriptionJson = SerializeToJson(ApimSubscription);
+
+            //    //Guid subscriptionId = Guid.NewGuid();
+
+            //    response = await client.PutAsync("/subscriptions/" + createSubscriptionId + "?api-version=" + _ApimRestApiVersion, new StringContent(ApimSubscriptionJson, Encoding.UTF8, "text/json"));
+            //    string httpContent = await response.Content.ReadAsStringAsync();
+
+
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        //Subscription created
+
+            //        //return Redirect(Request.QueryString["returnUrl"]);
+            //        return true; //Redirect("https://contosoinc.portal.azure-api.net/developer");
+            //    }
+            //    else
+            //    {
+            //        throw new HttpException((int)response.StatusCode, httpContent);
+            //    }
+            //}
+
+        }
+
+        public async Task<bool> RemoveProductSubscriptionAsync(string userId, string subscriptionId)
+        {
+            throw new NotImplementedException();
+
+            //////Register user for product
+            ////using (var client = new HttpClient())
+            ////{
+            ////    client.BaseAddress = new Uri(_ApimRestHost);
+            ////    client.DefaultRequestHeaders.Add("Authorization", ApimRestAuthHeader());
+            ////    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/json"));
+
+            ////    HttpResponseMessage response;
+
+            ////    client.DefaultRequestHeaders.Add("If-Match", "*");
+            ////    response = await client.DeleteAsync("/subscriptions/" + subscriptionId + "?api-version=" + _ApimRestApiVersion);
+
+
+            ////    if (response.IsSuccessStatusCode)
+            ////    {
+            ////        //Subscription created
+
+            ////        //return Redirect(Request.QueryString["returnUrl"]);
+            ////        return true; //return Redirect("https://contosoinc.portal.azure-api.net/developer");
+            ////    }
+            ////    else
+            ////    {
+            ////        throw new HttpException((int)response.StatusCode, await response.RequestMessage.Content.ReadAsStringAsync());
+            ////    }
+
+            ////}
         }
 
         #endregion
@@ -208,6 +395,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// Gets the policy configuration for the specified product.
         /// https://msdn.microsoft.com/en-us/library/azure/dn776336.aspx#GetPolicy
         /// </summary>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <param name="productId">Product identifier.</param>
         /// <returns></returns>
         public Task<bool> GetProductPolicyAsync(string productId, CancellationToken cancellationToken = default(CancellationToken))
@@ -225,6 +413,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// <summary>
         /// Determines if policy configuration is attached to the specified product.
         /// </summary>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <param name="productId">Product identifier.</param>
         /// <returns></returns>
         public Task<bool> CheckProductPolicyAsync(string productId, CancellationToken cancellationToken = default(CancellationToken))
@@ -242,6 +431,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// Sets the policy configuration for the specified product.
         /// https://msdn.microsoft.com/en-us/library/azure/dn776336.aspx#SetPolicy
         /// </summary>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <param name="productId">Product identifier.</param>
         /// <returns></returns>
         public Task<bool> SetProductPolicyAsync(string productId, CancellationToken cancellationToken = default(CancellationToken))
@@ -261,6 +451,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// <summary>
         /// Removes the policy configuration for the specified product.
         /// </summary>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <param name="productId">Product identifier.</param>
         /// <returns></returns>
         public Task<bool> DeleteProductPolicyAsync(string productId, CancellationToken cancellationToken = default(CancellationToken))
@@ -285,6 +476,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// Get a list of all APIs
         /// https://msdn.microsoft.com/en-us/library/azure/dn781423.aspx#ListAPIs
         /// </summary>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <returns></returns>
         public Task<List<API>> GetAPIsAsync(string filter = null, CancellationToken cancellationToken = default(CancellationToken))
         {
@@ -306,6 +498,7 @@ namespace SmallStepsLabs.Azure.ApiManagement
         /// Get a specific API
         /// https://msdn.microsoft.com/en-us/library/azure/dn776336.aspx#GetAPI
         /// </summary>
+        /// <exception cref="HttpResponseException">Thrown on invalid operation.</exception>
         /// <param name="apiId">API identifier.</param>
         /// <returns></returns>
         public Task<API> GetAPIAsync(string apiId, CancellationToken cancellationToken = default(CancellationToken))
@@ -326,10 +519,10 @@ namespace SmallStepsLabs.Azure.ApiManagement
         private void ValidateProduct(Product product)
         {
             if (String.IsNullOrEmpty(product.Name) && product.Name.Length > 100)
-                throw new InvalidProductException("");
+                throw new InvalidProductException("Product configuration is not valid. 'Name' is required and must not exceed 100 characters.");
 
             if (String.IsNullOrEmpty(product.Description) && product.Description.Length > 1000)
-                throw new InvalidProductException("");
+                throw new InvalidProductException("Product configuration is not valid. 'Description' is required and must not exceed 1000 characters.");
 
             // Cannot provide values for approvalRequired and subscriptionsLimit when subscriptionRequired is set to false in the request payload
             if (!product.SubscriptionRequired)
