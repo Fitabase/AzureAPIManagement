ab35574d76a7f82e3e4fbd393241b0bdc8c1c816
Aaron Coleman <aaron@fitabase.com>
2018-02-13 18:15:59 -0800

    more methods for policy

---
+++ Azure.ApiManagement.Client/DataModel/Model/Policy.cs
@@ -0,0 +1,22 @@
+﻿using Fitabase.Azure.ApiManagement.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitabase.Azure.ApiManagement.Model
+{
+    public class Policy : EntityBase
+    {
+        
+        /// <summary>
+        /// Policies as XML (same as in Azure Portal settings in Code View mode)
+        /// </summary>
+        [JsonProperty("policyContent")]
+        public string PolicyContent { get; set; }
+
+        protected override string UriIdFormat => "/policy";
+    }
+}
