16140775d6ba11f7a5f01b2bde6005ad009f337e
Brittany Schenk <brittany@fitabase.com>
2024-01-25 13:25:47 -0800

    Add properties collection to API model

---
+++ Azure.ApiManagement.Client/DataModel/Model/ApiProperties.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fitabase.Azure.ApiManagement.Model
+{
+    public class ApiProperties
+    {
+        [JsonProperty("displayName", NullValueHandling = NullValueHandling.Ignore)]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("apiRevision", NullValueHandling = NullValueHandling.Ignore)]
+        public string ApiRevision { get; set; }
+
+        [JsonProperty("description", NullValueHandling = NullValueHandling.Ignore)]
+        public string Description { get; set; }
+
+        [JsonProperty("serviceUrl", NullValueHandling = NullValueHandling.Ignore)]
+        public string ServiceUrl { get; set; }
+
+        [JsonProperty("path", NullValueHandling = NullValueHandling.Ignore)]
+        public string Path { get; set; }
+
+        [JsonProperty("protocols", NullValueHandling = NullValueHandling.Ignore)]
+        public List<string> Protocols { get; set; }
+
+        [JsonProperty("isCurrent", NullValueHandling = NullValueHandling.Ignore)]
+        public bool IsCurrent { get; set; }
+    }
+}
